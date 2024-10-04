8bc004b38b8aff898eeb0b8fc97cb1459a551a51
Adam Pangelinan <adamtpangelinan@gmail.com>
2024-10-04 19:43:14 +1000

    Change main.jsx imports for vercel build error logs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react';
-import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=e043a6c5";
 import { createRoot } from 'react-dom/client';
-import App from "/src/App.jsx";
-import "/src/index.css";
+import App from './App';
+import './index.css';
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
