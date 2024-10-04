4416321f5974d355b13f18774937a75b7ea7edb8
Adam Pangelinan <adamtpangelinan@gmail.com>
2024-10-04 19:37:41 +1000

    Fix frontend build commands, just some config errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=e043a6c5";
-const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import { createRoot } from 'react-dom/client';
 import App from "/src/App.jsx";
 import "/src/index.css";
 
