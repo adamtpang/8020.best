1626b54d3cc26a5e7079b55fb00d145939ba622b
Adam Pangelinan <adamtpangelinan@gmail.com>
2024-11-12 13:34:37 +1000

    Change price

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -155,12 +155,8 @@ const Landing = () => {
           {user && !hasPurchased && (
             <Box sx={{ mt: 4 }}>
               <stripe-buy-button
-                buy-button-id="buy_btn_1Q8WGpFL7C10dNyGiDnbvoQB"
+                buy-button-id="buy_btn_1QKAq3FL7C10dNyGNkAnNUXj"
                 publishable-key="pk_live_51J7Ti4FL7C10dNyGubXiYMWwF6jPahwvwDjXXooFE9VbI1Brh6igKsmNKAqmFoYflQveSCQ8WR1N47kowzJ1drrQ00ijl4Euus"
-                client-reference-id={user.email}
-                customer-email={user.email}
-                success-url={window.location.origin}
-                cancel-url={window.location.origin}
               >
               </stripe-buy-button>
             </Box>
