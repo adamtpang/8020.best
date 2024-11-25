abd87528c2e1f22f0fcafc126820916ee17a097d
Adam Pangelinan <adamtpangelinan@gmail.com>
2024-11-25 18:04:56 +1000

    Elonize hower

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,18 +1,24 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBWb19J68H1l6J0wbetZ5QUZ69JdEnMPDM",
+  authDomain: "howerdotapp.firebaseapp.com",
+  projectId: "howerdotapp",
+  storageBucket: "howerdotapp.firebasestorage.app",
+  messagingSenderId: "307357602530",
+  appId: "1:307357602530:web:74c747b9e861c0475304bb",
+  measurementId: "G-TZJTKDT7JC"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
 
-export default app;
+export { app, analytics };
+    
