6a964297084daeb0055a8d725f307005e5115953
Adam Pangelinan <adamtpangelinan@gmail.com>
2024-11-25 18:07:55 +1000

    Elonize hower

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBWb19J68H1l6J0wbetZ5QUZ69JdEnMPDM",
   authDomain: "howerdotapp.firebaseapp.com",
@@ -18,7 +15,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export { app, analytics };
+// Initialize Auth
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+// Export what we need
+export { auth, provider };
+export default app;
     
