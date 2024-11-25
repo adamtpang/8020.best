8792e0b480ce99bb685005ef3b6abf39073cc4e9
Adam Pangelinan <adamtpangelinan@gmail.com>
2024-11-25 11:54:33 +1000

    Fix landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Product from './components/Product';
 import Landing from './components/Landing';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
       <Routes>
         <Route path="/app" element={<Product />} />
         <Route path="/" element={<Landing />} />
