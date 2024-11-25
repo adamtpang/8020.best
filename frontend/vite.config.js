9fc520af2cdf29d99f4cd20092b2efbcb10011fc
Adam Pangelinan <adamtpangelinan@gmail.com>
2024-11-25 17:32:34 +1000

    Elonize hower

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/auth']
-    }
-  },
   optimizeDeps: {
-    include: ['axios']
+    include: ['axios', 'firebase/app', 'firebase/auth']
   },
   resolve: {
     alias: {
